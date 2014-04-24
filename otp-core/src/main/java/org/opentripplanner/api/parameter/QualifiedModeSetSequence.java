d37d56a66b2dbb0d7d37bf0dcf2cbacb401da6d8
Laurent Grégoire <laurent.gregoire@gmail.com>
2014-04-24 10:32:39 +0200

    Bike P+R: Apply qualified modes to the request.
    This code has been cherry-picked from qualified modes dev on 0.11.x.

--- otp-core/src/main/java/org/opentripplanner/api/parameter/QualifiedModeSetSequence.java
+++ otp-core/src/main/java/org/opentripplanner/api/parameter/QualifiedModeSetSequence.java
@@ -3,6 +3,10 @@ package org.opentripplanner.api.parameter;
 import java.util.List;
 import java.util.Set;
 
+import org.opentripplanner.routing.core.RoutingRequest;
+import org.opentripplanner.routing.core.TraverseMode;
+import org.opentripplanner.routing.core.TraverseModeSet;
+
 import com.beust.jcommander.internal.Sets;
 import com.google.common.collect.Lists;
 
@@ -11,10 +15,15 @@ import com.google.common.collect.Lists;
  * and wanted to park it and/or walk before taking a train or a tram, and finally rent a bicycle to
  * reach the destination: CAR_HAVE_PARK,WALK;TRAIN,TRAM;BIKE_RENT
  * It might also make sense to allow slashes meaning "or", or simply the word "or".
+ * 
+ * This class and QualifiedMode are clearly somewhat inefficient and allow nonsensical combinations like
+ * renting and parking a subway. They are not intended for use in routing. Rather, they simply parse the
+ * language of mode specifications that may be given in the mode query parameter. They are then converted
+ * into more efficient and useful representation in the routing request.
  */
 public class QualifiedModeSetSequence {
 
-    public List<Set<QualifiedMode>> qModeSets = Lists.newArrayList();
+    public List<Set<QualifiedMode>> sets = Lists.newArrayList();
     
     public QualifiedModeSetSequence(String s) {
         for (String seg : s.split(";")) {
@@ -23,9 +32,44 @@ public class QualifiedModeSetSequence {
                 qModeSet.add(new QualifiedMode(qMode));
             }
             if (!qModeSet.isEmpty()) {
-                qModeSets.add(qModeSet);
+                sets.add(qModeSet);
             }
-                
+
         }
     }
+    
+    /**
+     * Modify an existing routing request, setting fields to reflect these qualified modes.
+     * This is intended as a temporary solution, and uses the current system of a single mode set,
+     * accompanied by some flags to help with routing.
+     */
+    public void applyToRequest(RoutingRequest req) {
+        /* Start with an empty mode set. */
+        req.modes = new TraverseModeSet();
+        /* Use only the first set of qualified modes for now. */
+        if (sets.isEmpty()) return;
+        Set<QualifiedMode> qModes = sets.get(0);
+        // First, copy over all the modes
+        for (QualifiedMode qMode : qModes) {
+            req.modes.setMode(qMode.mode, true);
+        }
+        for (QualifiedMode qMode : qModes) {
+            if (qMode.mode == TraverseMode.BICYCLE) {
+                if (qMode.qualifiers.contains(Qualifier.RENT)) {
+                    req.allowBikeRental = true;
+                }
+                if (qMode.qualifiers.contains(Qualifier.PARK)) {
+                    req.bikeParkAndRide = true;
+                }
+            }
+            if (qMode.mode == TraverseMode.CAR) {
+                if (qMode.qualifiers.contains(Qualifier.PARK)) {
+                    req.parkAndRide = true;
+                } else {
+                    req.kissAndRide = true;
+                }
+            }
+        }
+    }
+
 }
