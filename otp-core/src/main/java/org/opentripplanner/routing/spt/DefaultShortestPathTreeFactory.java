3d8ca8b013aa52e22ab76e856e531ec14dfaa83d
Laurent Grégoire <laurent.gregoire@gmail.com>
2014-04-25 19:40:01 +0200

    Qualified modes: proper cond. for multi-state SPT.

--- otp-core/src/main/java/org/opentripplanner/routing/spt/DefaultShortestPathTreeFactory.java
+++ otp-core/src/main/java/org/opentripplanner/routing/spt/DefaultShortestPathTreeFactory.java
@@ -5,8 +5,8 @@ import org.opentripplanner.routing.core.RoutingRequest;
 /**
  * Default implementation of ShortestPathTreeFactory.
  * 
- * Creates a MultiShortestPathTree for any transit, bike/walk (Bike rental) or car/walk (P+R) trips,
- * otherwise uses BasicShortestPathTree.
+ * Creates a MultiShortestPathTree for any transit, bike rental, car/bike P+R, car kiss-and-ride.
+ * Otherwise uses BasicShortestPathTree.
  * 
  * @author avi
  */
@@ -15,9 +15,8 @@ public class DefaultShortestPathTreeFactory implements ShortestPathTreeFactory {
     @Override
     public ShortestPathTree create(RoutingRequest options) {
         ShortestPathTree spt = null;
-        if (options.getModes().isTransit() || options.getModes().getWalk()
-                && options.getModes().getBicycle() || options.getModes().getWalk()
-                && options.getModes().getCar()) {
+        if (options.getModes().isTransit() || options.allowBikeRental || options.bikeParkAndRide
+                || options.parkAndRide || options.kissAndRide) {
             spt = new MultiShortestPathTree(options);
         } else {
             spt = new BasicShortestPathTree(options);
